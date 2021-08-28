2923eb7c0f5fac0b9a2a58dcdbb8e4e0e6d87db9
Arian Hadi <arian.hadi78@gmail.com>
2021-08-28 15:19:49 +0430

    api initial

---
+++ API/Controllers/InputController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/Input/[action]")]
+    public class InputController : ControllerBase
+    {
+        
+    }
+}
