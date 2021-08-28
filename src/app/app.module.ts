45acebe0fda9f12490cd6fa60240bf0fc7beef88
milad nafar <milarnafar78+glab@gmail.com>
2021-08-28 13:25:02 +0430

    [ADD] pages directory and some component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { DatasetsComponent } from './pages/dashboard/datasets/datasets.component';
+import { ConnectionsComponent } from './pages/dashboard/connections/connections.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    DatasetsComponent,
+    ConnectionsComponent
   ],
   imports: [
     BrowserModule,
