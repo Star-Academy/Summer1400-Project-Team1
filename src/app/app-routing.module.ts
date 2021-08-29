6da13496e520eaed641b41b9b5391268d9a05ce1
milad nafar <milarnafar78+glab@gmail.com>
2021-08-29 13:09:17 +0430

    [ADD] connection route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {DashboardComponent} from './pages/dashboard/dashboard.component';
 import { PipelineComponent } from './pages/pipeline/pipeline.component';
 import {DatasetsComponent} from './pages/dashboard/datasets/datasets.component';
 import {AddNewDatasetComponent} from './pages/dashboard/datasets/add-new-dataset/add-new-dataset.component';
+import { ConnectionsComponent } from './pages/dashboard/connections/connections.component';
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent,
     children: [
       { path: 'datasets', component: DatasetsComponent },
+      { path: 'connection', component: ConnectionsComponent },
       { path: 'datasets/add', component: AddNewDatasetComponent }
 
     ]
