5985c4d1a0ee0df607e4d9d2e4350479fb0d9bd0
Irrfan Ghorbani <irrfanghorbani@gmail.com>
2021-08-29 14:32:07 +0430

    connection comp added

--- src/app/pages/dashboard/connections/connections.component.ts
+++ src/app/pages/dashboard/connections/connections.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Subscription} from 'rxjs';
+import { ConnectionRow} from 'src/app/modals/connection';
+import {ConnectionService} from 'src/app/services/connection.service';
 
 @Component({
   selector: 'app-connections',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnectionsComponent implements OnInit {
 
-  constructor() { }
+  connectionsRows!:ConnectionRow[];
+  connectionRowsSub!: Subscription;
+  displayedColumns: string[] = ['شماره', 'نام اتصال', 'تاریخ ساخت'];
+
+  constructor(public connectionService:ConnectionService) { }
 
   ngOnInit(): void {
+    this.connectionService.getConnection();
+    this.connectionsRows = this.connectionService.connectionRows;
+    this.connectionRowsSub = this.connectionService.connectionRowsChanged.subscribe((connectionRows: ConnectionRow[]) => {
+        this.connectionsRows = connectionRows;
+     });
   }
+
+  onConnectionClick(row:ConnectionRow){
+    console.log(row.connection.name);
+  }
+
+  ngOnDestroy(): void {
+    this.connectionRowsSub
+    .unsubscribe();
+}
 
 }
