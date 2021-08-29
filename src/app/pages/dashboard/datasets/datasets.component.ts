4df6c5210e0463d161903ca99ab799a5e801a6da
milad nafar <milarnafar78+glab@gmail.com>
2021-08-29 13:11:58 +0430

    [ADD] datasets table

--- src/app/pages/dashboard/datasets/datasets.component.ts
+++ src/app/pages/dashboard/datasets/datasets.component.ts
@@ -1,26 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Dataset } from 'src/app/modals/dataset';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import { DatasetRow} from 'src/app/modals/dataset';
+import {DatasetService} from 'src/app/services/dataset.service';
 
 @Component({
-  selector: 'app-datasets',
-  templateUrl: './datasets.component.html',
-  styleUrls: ['./datasets.component.scss']
+    selector: 'app-datasets',
+    templateUrl: './datasets.component.html',
+    styleUrls: ['./datasets.component.scss'],
 })
-export class DatasetsComponent implements OnInit {
+export class DatasetsComponent implements OnInit, OnDestroy {
 
-  datasets=[
-      new Dataset(),
-      new Dataset(),
-      new Dataset(),
-      new Dataset(),
-  ]
-  constructor() { }
+    datasetsRows!: DatasetRow[];
+    datasetsRowsSub!: Subscription;
 
-  ngOnInit(): void {
-  }
+    displayedColumns: string[] = ['شماره', 'نام پایگاه', 'نام اتصال', 'تاریخ ساخت'];
+
+    constructor(private datasetService: DatasetService) {}
+
+    ngOnInit(): void {
+        this.datasetService.getDatasets();
+        this.datasetsRows = this.datasetService.datasetsRows;
+        this.datasetsRowsSub = this.datasetService.datasetsRowsChanged.subscribe((datasetsRows: DatasetRow[]) => {
+            this.datasetsRows = datasetsRows;
+         });
+    }
 
     onUpload(event: any) {
-      if(event.target !== null)
-        console.log(event.target.files);
+        if (event.target !== null)
+            console.log(event.target.files);
+    }
+
+    onDatasetClick(row: DatasetRow) {
+        console.log(row.dataset.name);
+     }
+
+
+    ngOnDestroy(): void {
+        this.datasetsRowsSub.unsubscribe();
     }
 }
