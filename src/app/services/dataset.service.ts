a23c9183da9c719ef517f3a1826714dbca075590
milad nafar <milarnafar78+glab@gmail.com>
2021-08-29 13:11:03 +0430

    [ADD] dataset service

---
+++ src/app/services/dataset.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from '@angular/core';
+import {Dataset, DatasetRow} from '../modals/dataset';
+import {Subject} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatasetService {
+
+  datasetsTemp:Dataset[]=[
+    new Dataset(),
+    new Dataset(),
+    new Dataset(),
+    new Dataset()
+  ]
+
+  private _datasets!:Dataset[];
+  private _datasetsRows!:DatasetRow[];
+
+  datasetsChanged = new Subject<Dataset[]>();
+  datasetsRowsChanged = new Subject<DatasetRow[]>();
+
+  get datasets(): Dataset[] {
+    return this._datasets;
+  }
+  set datasets(value: Dataset[]) {
+    this._datasets = value;
+    this.datasetsChanged.next(value);
+    this.datasetsRows = value.map((dataSet: Dataset, index) => {
+      return new DatasetRow(index+1, dataSet);
+    });
+  }
+  get datasetsRows(): DatasetRow[] {
+    return this._datasetsRows;
+  }
+
+  set datasetsRows(value: DatasetRow[]) {
+    this._datasetsRows = value;
+    this.datasetsRowsChanged.next(value);
+  }
+  constructor() {
+   }
+   getDatasets(){
+     this.datasets = this.datasetsTemp;
+  }
+
+}
