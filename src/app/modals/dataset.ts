0a9533948d3d29e7e9e5023ed7ea47bcdb89cf95
milad nafar <milarnafar78+glab@gmail.com>
2021-08-29 09:02:19 +0430

    [ADD] dataset model

---
+++ src/app/modals/dataset.ts
@@ -0,0 +1,7 @@
+export class  Dataset{
+    constructor(public id:number =-1,
+                public name:string="testName",
+                public connectionName:string="testConnectionName",
+                public createdAt:string="testDate")
+    {}
+}
