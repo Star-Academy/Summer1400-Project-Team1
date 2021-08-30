1475ea0c19bc89c46672127ab9e017950150f6f0
Saleh-Safarnejad <saleh.safarnejad@gmail.com>
2021-08-31 03:04:16 +0430

    handle pipeline preview resizing

--- src/app/pages/pipeline/pipeline.component.ts
+++ src/app/pages/pipeline/pipeline.component.ts
@@ -14,14 +14,33 @@ export class PipelineComponent implements OnInit {
   expandSidebar = true;
   expandPreview = true;
   isModalOpen=false;
-  
+  previewResize = {
+    isResizing: false,
+    previewHeight: 300,
+    lastYPosition: 0
+  }
+
   constructor(public router: Router) { }
 
   ngOnInit(): void {
+    document.onmouseup = (event) => {this.previewResize.isResizing = false;}
   }
 
   editPipelineName(ngForm: NgForm) {
     this.pipelineTitle = ngForm.value.newTitle
     this.isEditingPipelineTitle = false;
+  }
+
+  onStartResize(event: MouseEvent) {
+    this.previewResize.isResizing = true;
+    this.previewResize.lastYPosition = event.pageY;
+  }
+
+  handleResize(event: MouseEvent){
+    if (this.previewResize.isResizing){
+      let heightOffset = this.previewResize.lastYPosition - event.pageY;
+      this.previewResize.previewHeight += heightOffset;
+      this.previewResize.lastYPosition = event.pageY
+    }
   }
 }
