582aa32532db4f1e3d52abdbd15636a13e0b14f2
milad nafar <milarnafar78+glab@gmail.com>
2021-09-01 02:42:21 +0430

    [ADD] sidebar component

--- src/app/pages/pipeline/sidebar/sidebar.component.ts
+++ src/app/pages/pipeline/sidebar/sidebar.component.ts
@@ -1,14 +1,29 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { NodeType } from "src/app/modals/node";
+import {Component, Input, OnInit, Output} from "@angular/core";
+import {Node, NodeType} from "src/app/modals/node";
+import {ProcessorDialogComponent} from "../pipeline-graph/node-item/processor-dialog/processor-dialog.component";
+import {Alert} from "../../../utils/alert";
+import {PipelineService} from "../../../services/pipeline.service";
+import {MatDialog} from "@angular/material/dialog";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {EventEmitter} from "@angular/core";
 
 @Component({
-  selector: "app-sidebar",
-  templateUrl: "./sidebar.component.html",
-  styleUrls: ["./sidebar.component.scss"],
+    selector: "app-sidebar",
+    templateUrl: "./sidebar.component.html",
+    styleUrls: ["./sidebar.component.scss"],
 })
 export class SidebarComponent implements OnInit {
-  @Input() processType!: string;
-  constructor() {}
+    @Input() processType!: string;
+    @Output() openChooseProcessorDialog = new EventEmitter<void>();
 
-  ngOnInit(): void {}
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+
+    openDialog() {
+        this.openChooseProcessorDialog.emit();
+    }
 }
