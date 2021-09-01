d9f0904ee80b83461b5922b1109a8ac706dc43df
AlisaLC <ali_sal1381@yahoo.com>
2021-09-01 20:18:26 +0430

    JoinTask implements IPipelineComponent

--- API/Join/JoinTask.cs
+++ API/Join/JoinTask.cs
@@ -1,37 +1,34 @@
-using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace API.Join
 {
-    public class JoinTask : IJoinTask
+    public class JoinTask : IPipelineComponent
     {
         private readonly ISqlHandler _sqlHandler;
-        private readonly string _firstTableName, _secondTableName;
+        private readonly string _secondTableName;
         private readonly string _firstTablePk, _secondTablePk;
         private readonly IDictionary<string, string> _firstTableMapping, _secondTableMapping;
         private readonly JoinType _joinType;
 
-        public JoinTask(ISqlHandler sqlHandler, string firstTableName, string secondTableName, JoinType joinType,
+        public JoinTask(ISqlHandler sqlHandler, string secondTableName, JoinType joinType,
             string firstTablePk,
             string secondTablePk)
         {
             _sqlHandler = sqlHandler;
-            _firstTableName = firstTableName;
             _secondTableName = secondTableName;
             _joinType = joinType;
             _firstTablePk = firstTablePk;
             _secondTablePk = secondTablePk;
         }
 
-        public JoinTask(ISqlHandler sqlHandler, string firstTableName, string secondTableName, JoinType joinType,
+        public JoinTask(ISqlHandler sqlHandler, string secondTableName, JoinType joinType,
             string firstTablePk,
             string secondTablePk, IDictionary<string, string> firstTableMapping,
             IDictionary<string, string> secondTableMapping)
         {
             _sqlHandler = sqlHandler;
-            _firstTableName = firstTableName;
             _secondTableName = secondTableName;
             _joinType = joinType;
             _firstTablePk = firstTablePk;
@@ -40,27 +37,41 @@ namespace API.Join
             _secondTableMapping = secondTableMapping;
         }
 
-        public DataTable Run()
+        public string Execute(string sourceDataset)
         {
-            string sqlString = GetSqlString();
+            string tempTableName = GetTempTableNameFromSourceDataset(sourceDataset);
+            string sqlString = GetSqlString(sourceDataset, tempTableName);
             if (!_sqlHandler.IsOpen())
                 _sqlHandler.Open();
             SqlCommand command = new SqlCommand(sqlString, _sqlHandler.Connection);
-            var stream = command.ExecuteReader();
-            DataTable table = new DataTable();
-            table.Load(stream);
-            _sqlHandler.Close();
-            return table;
+            command.ExecuteNonQuery();
+            return tempTableName;
         }
 
-        private string GetSqlString()
+        private static string GetTempTableNameFromSourceDataset(string sourceDataset)
+        {
+            string tempTableName = sourceDataset;
+            Match match = Regex.Match(tempTableName, "(\\w+)_temp(\\d+)");
+            if (match.Success)
+            {
+                tempTableName = match.Groups[1].Value + "_temp" + (int.Parse(match.Groups[2].Value) + 1);
+            }
+            else
+            {
+                tempTableName += "_temp1";
+            }
+
+            return tempTableName;
+        }
+
+        private string GetSqlString(string sourceDataset, string tempTableName)
         {
             string sqlString = "SELECT ";
-            sqlString = AddMappingToSqlString(sqlString, _firstTableName, _firstTableMapping) + ", ";
+            sqlString = AddMappingToSqlString(sqlString, sourceDataset, _firstTableMapping) + ", ";
             sqlString = AddMappingToSqlString(sqlString, _secondTableName, _secondTableMapping);
-            sqlString += " FROM " + _firstTableName + " ";
+            sqlString += " INTO ##" + tempTableName + " FROM " + sourceDataset + " ";
             sqlString += _joinType.ToString().Replace("Join", "").ToUpper() + " JOIN " + _secondTableName + " ON ";
-            sqlString += _firstTableName + "." + _firstTablePk + " = " + _secondTableName + "." + _secondTablePk;
+            sqlString += sourceDataset + "." + _firstTablePk + " = " + _secondTableName + "." + _secondTablePk;
             return sqlString;
         }
 
@@ -91,5 +102,13 @@ namespace API.Join
 
             return sqlString;
         }
+    }
+
+    public enum JoinType
+    {
+        InnerJoin,
+        LeftJoin,
+        RightJoin,
+        FullJoin
     }
 }
