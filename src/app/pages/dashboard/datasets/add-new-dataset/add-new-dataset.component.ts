dd4370422a4ce77ddb3b945975219ea0d8263cbc
Irrfan Ghorbani <irrfanghorbani@gmail.com>
2021-09-09 19:43:48 +0430

    add routing to button add new dataset

--- src/app/pages/dashboard/datasets/add-new-dataset/add-new-dataset.component.ts
+++ src/app/pages/dashboard/datasets/add-new-dataset/add-new-dataset.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { Location } from "@angular/common";
 import { NgForm } from "@angular/forms";
 import { StoredDataService } from "src/app/services/stored-data.service";
+import { Router } from "@angular/router";
+import {Connection,ConnectionRow} from "../../../../models/connection";
 
 @Component({
   selector: "app-add-new-dataset",
@@ -13,16 +15,25 @@ export class AddNewDatasetComponent implements OnInit {
   datasetName!: string;
   panelOpenState: boolean = false;
   isLocalHost: boolean = false;
+  hasName:boolean=false;
   initDatasetName = "";
+  templist=[
+  new Connection(1,"test1","28/5"),
+  new Connection(2,"test2","28/5"),
+  new Connection(3,"test3","28/5"),
+  new Connection(4,"test4","28/5")];
+
   constructor(
     private storedDataService: StoredDataService,
-    private location: Location
+    private location: Location,
+    private router: Router
   ) {}
 
   ngOnInit() {
     if (this.storedDataService.datasetFile) {
       this.initDatasetName = this.storedDataService.datasetFile.name;
       this.isLocalHost = true;
+      this.hasName = true;
     }
   }
 
@@ -37,5 +48,9 @@ export class AddNewDatasetComponent implements OnInit {
 
   onUpload(event: any) {
     if (event.target !== null) console.log(event.target.files);
+  }
+
+  addNewConnection(){
+    this.router.navigateByUrl("dashboard/connection/add"); 
   }
 }
