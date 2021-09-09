1e58809de1768b5af426f4177c1eb5386e95ff0c
Saleh-Safarnejad <saleh.safarnejad@gmail.com>
2021-09-09 14:17:24 +0430

    modify filter node class

--- src/app/models/graph/processor-nodes/filter-node.ts
+++ src/app/models/graph/processor-nodes/filter-node.ts
@@ -2,7 +2,29 @@ import { Node, NodeType } from "../node";
 import { ProcessorNode } from "./processor-node";
 
 export class FilterNode extends ProcessorNode {
+  root = new FilterOperator(null);
   constructor(name: string) {
     super(name, NodeType.FILTER);
   }
+}
+
+abstract class Filter {
+  constructor(parent: FilterOperator | null) {}
+}
+
+class FilterOperator extends Filter {
+  operator: "AND" | "OR" = "AND";
+  children: Filter[] = [];
+}
+
+class FilterOperand extends Filter {
+  key!: string;
+  operator!: Operator;
+  value!: string;
+}
+
+enum Operator {
+  LESS_THAN = ">",
+  GREATER_THAN = "<",
+  EQUAL_TO = "==",
 }
